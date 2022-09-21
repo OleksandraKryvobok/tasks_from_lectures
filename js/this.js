47de267bea64cb297541b2bba9a739271592ffea
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2022-09-21 15:09:24 +0300

    add 3 task

--- js/this.js
+++ js/this.js
@@ -1,3 +1,5 @@
+//-----------------1-----------------
+
 // const counter = {
 //     value: 0,
 //     increment(value) {
@@ -19,33 +21,59 @@
 // updateCounter(5, counter.decrement.bind((counter)));
 // console.log(counter.value);
 
-const counter = {
-    value: 0,
-    decreaseValue() {
-        this.value -= 1;
-    },
-    increaseValue() {
-        this.value += 1;
-    }
-};
+//-----------------2-----------------
 
-const refs = {
-    decreaseBtn: document.querySelector('.js-decrement'),
-    increaseBtn: document.querySelector('.js-increment'),
-    valueEl: document.querySelector('.js-value'),
-};
+// const counterSec = {
+//     value: 0,
+//     decreaseValue() {
+//         this.value -= 1;
+//     },
+//     increaseValue() {
+//         this.value += 1;
+//     }
+// };
 
-// console.log(refs.value.classList);
+// const refs = {
+//     decreaseBtn: document.querySelector('.js-decrement'),
+//     increaseBtn: document.querySelector('.js-increment'),
+//     valueEl: document.querySelector('.js-value'),
+// };
+
+// // console.log(refs.value.classList);
 
 
 
-refs.decreaseBtn.addEventListener('click', () =>{
-    counter.decreaseValue();
-    refs.valueEl.textContent = counter.value;
-});
-refs.increaseBtn.addEventListener('click', () => {
-    counter.increaseValue();
-    refs.valueEl.textContent = counter.value;
-});
+// refs.decreaseBtn.addEventListener('click', () =>{
+//     counterSec.decreaseValue();
+//     refs.valueEl.textContent = counter.value;
+// });
+// refs.increaseBtn.addEventListener('click', () => {
+//     counterSec.increaseValue();
+//     refs.valueEl.textContent = counter.value;
+// });
+
+/*-----------------3-----------------
+we have Call, Apply, but no Bind. Create your own Bind
+*/
+
+// function sayHello(a, b) {
+//     console.log(`Hello, ${this.name}!`, a + b);
+// }
+
+// const obj1 = { name: 'Vanya' };
+// const obj2 = { name: 'Dima' };
+
+// const fn1 = sayHello.bind(obj1);
+// fn1(5, 8);
+
+// function myBind(context, callback) {
+//     return function(){
+//         callback.apply(context, arguments);
+//     }
+// }
+
+// const fn2 = myBind(obj2, sayHello);
+
+// fn2(2, 2);
 
 
